637b2f253f69712e7ed02c886a76cb01e047837c
Kyle Brandt <kyle@kbrandt.com>
2016-02-07 11:48:14 -0500

    read from config

--- server/main.go
+++ server/main.go
@@ -3,19 +3,35 @@ package main
 import (
 	"log"
 
-	"github.com/kylebrandt/annotate/web"
+	"github.com/BurntSushi/toml"
 	"github.com/kylebrandt/annotate/backend"
+	"github.com/kylebrandt/annotate/web"
 )
 
+type Conf struct {
+	ListenAddress   string
+	ElasticClusters []ElasticCluster
+}
 
+type ElasticCluster struct {
+	Servers []string // i.w. http://ny-elastic01:9200
+	Index   string
+}
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	b, err := backend.NewElastic([]string{"http://ny-devlogstash04:9200"}, "annotate")
-	if err != nil {
-		log.Fatal(err)
+	var c Conf
+	if _, err := toml.DecodeFile("./config.toml", &c); err != nil {
+		log.Fatal("failed to decode config file: ", err)
 	}
-	backends := []backend.Backend{b}
+	backends := []backend.Backend{}
+	for _, eCluster := range c.ElasticClusters {
+		b, err := backend.NewElastic(eCluster.Servers, eCluster.Index)
+		if err != nil {
+			log.Fatal(err)
+		}
+		backends = append(backends, b)
+	}
 	for _, b := range backends {
 		if err := b.InitBackend(); err != nil {
 			log.Fatal(err)
